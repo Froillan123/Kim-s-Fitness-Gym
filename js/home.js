fb7850e54320aa5c9da39df31622bbf430ea4082
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2023-11-04 00:32:23 +0800

    Lazy

--- js/home.js
+++ js/home.js
@@ -13,3 +13,19 @@ var swiper = new Swiper(".home-slider", {
         clickable: true,
     },
 });
+
+// Define an array of image URLs you want to cycle through
+const imageUrls = ["images/02.jpg", "images/about2.jpg", "images/about1.png"];
+
+const imgElement = document.getElementById("about-img");
+let currentImageIndex = 0;
+
+function changeImage() {
+    imgElement.src = imageUrls[currentImageIndex];
+    currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
+}
+
+// Change the image every 5 seconds
+setInterval(changeImage, 5000);
+
+
