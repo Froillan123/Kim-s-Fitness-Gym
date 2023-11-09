bc6790b78b03dd2f6757fcd27e4f1aedc6dfc10a
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2023-11-09 21:24:11 +0800

    temp

--- js/home.js
+++ js/home.js
@@ -25,79 +25,92 @@ function changeImage() {
     currentImageIndex = (currentImageIndex + 1) % imageUrls.length;
 }
 
-// Change the image every 5 seconds
-setInterval(changeImage, 3000);
+const carousel = document.querySelector(".card");
+        const cardWidth = carousel.querySelectorAll(".card").offsetWidth;
+        let currentCardIndex = 0;
+        const maxScreenWidth = 0; // Maximum screen width for full card width
 
+        function changeCard() {
+            currentCardIndex = (currentCardIndex + 1) % carousel.children.length;
+            const scrollOffset = currentCardIndex * cardWidth;
+            carousel.scrollTo({
+                left: scrollOffset,
+                behavior: 'smooth',
+            });
+        }
 
-const wrapper = document.querySelector(".wrapper");
-const carousel = document.querySelector(".carousel");
-const firstCardWidth = carousel.querySelector(".card").offsetWidth;
-const arrowBtns = document.querySelectorAll(".wrapper i");
-const carouselChildrens = [...carousel.children];
-let isDragging = false, isAutoPlay = true, startX, startScrollLeft, timeoutId;
-// Get the number of cards that can fit in the carousel at once
-let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
-// Insert copies of the last few cards to beginning of carousel for infinite scrolling
-carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
-    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-});
-// Insert copies of the first few cards to end of carousel for infinite scrolling
-carouselChildrens.slice(0, cardPerView).forEach(card => {
-    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
-});
-// Scroll the carousel at appropriate postition to hide first few duplicate cards on Firefox
-carousel.classList.add("no-transition");
-carousel.scrollLeft = carousel.offsetWidth;
-carousel.classList.remove("no-transition");
-// Add event listeners for the arrow buttons to scroll the carousel left and right
-arrowBtns.forEach(btn => {
-    btn.addEventListener("click", () => {
-        carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+        setInterval(changeCard, 2000);
+
+let currentIndex = 0; 
+
+function scrollCarousel(direction) {
+    const cardWidth = document.querySelector('.card').offsetWidth;
+    const carousel = document.querySelector('.carousel');
+    const maxIndex = carousel.childElementCount - 1;
+    const containerWidth = carousel.offsetWidth;
+    const maxScrollLeft = cardWidth * maxIndex - containerWidth;
+    
+    if (direction === 'next' && currentIndex < maxIndex) {
+        currentIndex++;
+    } else if (direction === 'prev' && currentIndex > 0) {
+        currentIndex--;
+    } else if (direction === 'next' && currentIndex === maxIndex) {
+        // If at the end, jump to the beginning (circular)
+        currentIndex = 0;
+    } else if (direction === 'prev' && currentIndex === 0) {
+        // If at the beginning, jump to the end (circular)
+        currentIndex = maxIndex;
+    }
+
+    const scrollLeft = currentIndex * cardWidth;
+    carousel.scrollTo({
+        left: scrollLeft,
+        behavior: 'smooth',
     });
-});
-const dragStart = (e) => {
-    isDragging = true;
-    carousel.classList.add("dragging");
-    // Records the initial cursor and scroll position of the carousel
-    startX = e.pageX;
-    startScrollLeft = carousel.scrollLeft;
 }
-const dragging = (e) => {
-    if(!isDragging) return; // if isDragging is false return from here
-    // Updates the scroll position of the carousel based on the cursor movement
-    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
-}
-const dragStop = () => {
-    isDragging = false;
-    carousel.classList.remove("dragging");
-}
-const infiniteScroll = () => {
-    // If the carousel is at the beginning, scroll to the end
-    if(carousel.scrollLeft === 0) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
-        carousel.classList.remove("no-transition");
-    }
-    // If the carousel is at the end, scroll to the beginning
-    else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth) {
-        carousel.classList.add("no-transition");
-        carousel.scrollLeft = carousel.offsetWidth;
-        carousel.classList.remove("no-transition");
-    }
-    // Clear existing timeout & start autoplay if mouse is not hovering over carousel
-    clearTimeout(timeoutId);
-    if(!wrapper.matches(":hover")) autoPlay();
-}
-const autoPlay = () => {
-    if(window.innerWidth < 400 || !isAutoPlay) return; // Return if window is smaller than 800 or isAutoPlay is false
-    // Autoplay the carousel after every 2500 ms
-    timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 2000);
-}
-autoPlay();
-carousel.addEventListener("scroll", infiniteScroll);
-wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
-wrapper.addEventListener("mouseleave", autoPlay);
 
+$('.carousel').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+      
+  
+
+document.addEventListener('DOMContentLoaded', () => {
+    const prevButton = document.querySelector('.prev-button');
+    const nextButton = document.querySelector('.next-button');
+
+    prevButton.addEventListener('click', () => scrollCarousel('prev'));
+    nextButton.addEventListener('click', () => scrollCarousel('next'));
+});
 document.addEventListener('DOMContentLoaded', function() {
 ScrollReveal({ 
     reset: true,
@@ -109,5 +122,5 @@ ScrollReveal({
 
 ScrollReveal().reveal('.home-section2, .heading, ', { origin: 'top' });
 ScrollReveal().reveal('.container, .about-content, .wrapper, .home',  { origin: 'bottom' });
-ScrollReveal().reveal('.about-img',  { origin: 'left' });
+ScrollReveal().reveal('.about-img, .testimonials',  { origin: 'left' });
 });
